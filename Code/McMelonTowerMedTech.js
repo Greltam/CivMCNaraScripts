d9fcfa0ffd4bddf42bea854584f5e7a8648ec0ae
Greltam <101361088+Greltam@users.noreply.github.com>
2024-04-28 20:55:16 -0400

    Update McMelonTowerMedTech.js
    Adjusted some comment text to elaborate on starting conditions.

--- Code/McMelonTowerMedTech.js
+++ Code/McMelonTowerMedTech.js
@@ -1,45 +1,20 @@
-
-
-/*
-    
-    Location: 4000, 7400, y104
-    Size: 48 stacks
-    Harvest Time: 30 minutes
-    NL: ZealFarm owned by Greltam
-    Tool: E5U3ST Axe, Left click break
-    tab-outable
-    
-*/
-
-
-
-
 //Zeal River Savannah Melon script
 /*
     !!! Script starts at 3987, 7374, 104 !!!
     Find 1.2 Player Configurables to adjust for script restarts
 
-    Carpet Collection Script for Meenos on CivMC @ 2958, 5202, 67
+    Savanna Melon Tower Script on CivMC @ 2958, 5202, 67
     Written by Greltam 4/9/2024
-    
-    With a largely expanded tree farm, I need a script I won't have to baby every hour
-    for 8+hours that it can run. The old script only ever chopped or replanted 1 layer
-    at a time.
-    
-    The idea is: start at a lodestone. move west to chopping start. For each time
-    down the row, toss wood into the collectors. At the end of the layer, jump up
-    a lodestone to the next layer, and repeat until finishing the top of the farm.
 
-    I can write a similar script for replanting after this is done the heavy lifting.
-    
-    New Tech being written to swap out tools near breaking to new ones so script can
-    fully run
+    Tab-outable.
 */
 /*------------------------
    0.1 Player Requirements to Start
 ------------------------*/
+
 //Directions: Start script while standing on that layers lodestone
-//Player must place axe in the rightmost hotbar(9)
+//Start with Axe in mainhand
+//Add 1-2 extra axes in inventory, script will replace at 10 durability.
 
 /*-----------------------
    0.1 Player Requirements to Start End
@@ -180,7 +155,7 @@ KeyBind.key("key.mouse.right", false)
 KeyBind.key("key.mouse.left", false)
 
 
-Chat.log("Meloning Accomplished, we're going home Boys!")
+Chat.log("Melon Accomplished, we're going home Boys!")
 
 /*-------------------
    4 Program End
