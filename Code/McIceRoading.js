59cdd77804bebe6d59fced5d1d8d751fb91b9d7a
Greltam <101361088+Greltam@users.noreply.github.com>
2024-05-03 10:36:19 -0400

    Update McIceRoading.js
    Brought the ice roading script up to more modern standards, added snapping to cardinal location.

--- Code/McIceRoading.js
+++ Code/McIceRoading.js
@@ -1,86 +1,107 @@
-// Walking is a pain, let automation take the pain away!
+//Zeal Ice Roading Script
 /*
-    Walking script
-    Written by Greltam 8/28/2022
+
+    Zeal Ice Roading Script on CivMC
+    ReWritten by Greltam 5/3/2024
     
-    Player can adjust script to either walk, run, jump, eat,
-    and iceroad(a combination of all) as they need. 
-    Have food as your item selected if eating is toggled to true
 */
-
 /*------------------------
-    Global Variables Start
+   0.1 Player Requirements to Start
 ------------------------*/
-// true
-// false
-//iceroading. Player needs to sprint, jump, and eat to traverse iceroads.
-//will toggle sprinting, jumping, and eating to true regardless of other variables
-iceroading = true
 
-//enable sprinting
-sprinting = false
-//enable jumping
-jumping = false
-//enable eating
-eating = false
-//short circuit key to quit crafting early
-quitKey = "key.keyboard.s"
+//Directions: Look in the general direction you wish to iceroad
+//Player will snap to a cardinal looking position
+//Have food in mainhand
 
 /*-----------------------
-    Global Variables End
+   0.1 Player Requirements to Start End
+-----------------------*/
+
+
+/*------------------------
+   1.1 Import Files Start
+------------------------*/
+const util = require("./McUtilityFile.js")
+/*-----------------------
+   1.1 Import Files End
+-----------------------*/
+
+/*------------------------
+   1.2 Player Configurables Start
+------------------------*/
+
+//alter the default quitkey from s to whatever you want.
+util.setQuitKey("key.keyboard.s") //default: util.setQuitKey("key.keyboard.s")
+
+//default movement keys
+sprintKey = "key.keyboard.left.control"   //default: "key.keyboard.left.control" 
+forwardKey = "key.keyboard.w"             //default: "key.keyboard.w"
+useKey = "key.mouse.right"                //default: "key.mouse.right" 
+jumpKey = "key.keyboard.space"            //default: "key.keyboard.space"
+
+/*-----------------------
+   1.2 Player Configurables End
+-----------------------*/
+
+/*------------------------
+   2 Global Variables Start
+------------------------*/
+
+/*-----------------------
+   2 Global Variables End
 -----------------------*/
 
 /*-------------------
-    Functions Start
+   3 Functions Start
 -------------------*/
-
-function checkQuit(){
-    if(KeyBind.getPressedKeys().contains(quitKey)){
-        return true
+function snapToCardinal(){
+    playerYaw = Player.getPlayer().getYaw()
+    if(playerYaw < 45 && playerYaw > -45){
+        player.lookAt(0,0)
     }
-    return false
-}
-/*-------------------
-    Functions End
--------------------*/
-
-/*-------------------
-    Program Start
--------------------*/
-
-
-Chat.log("Forward start")
-Chat.log("Press: " + quitKey + " to end script")
-while(!checkQuit()){
-    if(sprinting || iceroading){
-        KeyBind.key("key.keyboard.left.control", true)
+    else if(playerYaw < -45 && playerYaw > -135){
+        player.lookAt(-90,0)
     }
-    
-    KeyBind.key("key.keyboard.w", true)
-    
-    if(eating || iceroading){
-        KeyBind.key("key.mouse.right", true)
-    }
-    if(jumping || iceroading){
-        KeyBind.key("key.keyboard.space", true)
-        Client.waitTick(1)
-        KeyBind.key("key.keyboard.space", false)
-        Client.waitTick(1)
+    else if(playerYaw < 135 && playerYaw > 45){
+        player.lookAt(90,0)
     }
     else{
-        Client.waitTick(1)
+        player.lookAt(180,0)    
     }
-    
 }
-
-//turn off bound keys to exit script
-KeyBind.key("key.keyboard.w", false)
-KeyBind.key("key.keyboard.left.control", false)
-KeyBind.key("key.keyboard.space", false)
-KeyBind.key("key.mouse.right", false)
-
-Chat.log("Forward over")
+/*-------------------
+   3 Functions End
+-------------------*/
 
 /*-------------------
-    Program End
+   4 Program Start
+-------------------*/
+
+Chat.log("Zeal Ice Roader, boosting Nitro!!")
+Chat.log("Press: " + util.getQuitKey() + " to end script")
+
+snapToCardinal()
+
+while(!util.checkQuit()){
+    
+    KeyBind.key(sprintKey, true)
+    KeyBind.key(forwardKey, true)
+    KeyBind.key(useKey, true)
+    
+    KeyBind.key(jumpKey, true)
+    util.spinTicks(1)
+    KeyBind.key(jumpKey, false)
+    util.spinTicks(1)
+}
+
+//util.resetKeyBinds()
+    KeyBind.key(sprintKey, false)
+    KeyBind.key(forwardKey, false)
+    KeyBind.key(useKey, false)
+    KeyBind.key(jumpKey, false)
+
+Chat.log("Out of Nitro. ")
+
+/*-------------------
+   4 Program End
 -------------------*/
