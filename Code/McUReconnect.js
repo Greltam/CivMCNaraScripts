2a859fdfb536c8cb787a1183c0ed577f8efbc8c2
Greltam <101361088+Greltam@users.noreply.github.com>
2025-09-05 12:07:54 -0400

    Added farm auto-restart

--- Code/McUReconnect.js
+++ Code/McUReconnect.js
@@ -29,14 +29,100 @@
    3 Functions End
 -------------------*/
 
+function insideOf(x1,z1,x2,z2,x,z){
+
+    if(x >= x1 &&
+       x <= x2 &&
+       z >= z1 &&
+       z <= z2){
+       
+       return true
+    }
+
+    return false
+}
+
+function locateFarm(x,z){
+    
+    //Coords for Oak farm
+    if(insideOf(2863,5114,2959,5203,x,z)){
+        return "McOakHiTechChop.js"
+    }
+    
+    //Coords for Carrot farm
+    if(insideOf(2974,5105,3007,5138,x,z)){
+        return "McCarrotTower.js"
+    }
+    
+    //Coords for Wheat farm
+    if(insideOf(3021,5105,3055,5139,x,z)){
+        return "McWheatTower.js"
+    }
+    
+    //Coords for McBeachaEastMelonTower farm
+    if(insideOf(3994,7337,4021,7380,x,z)){
+        return "McBeachaEastMelonTower.js"
+    }
+    
+    //Coords for Potato Tower farm
+    if(insideOf(3994,7386,4024,7415,x,z)){
+        return "McPotatoTower.js"
+    }
+    
+    //Coords for Sweet berry farm
+    if(insideOf(3949,7426,3984,7457,x,z)){
+        return "McSweetBerry.js"
+    }
+    
+    //Coords for Zeal Melon farm
+    if(insideOf(3952,7372,3987,7390,x,z)){
+        return "McZealWestMelonTower.js"
+    }
+    
+    //Coords for Cocoa Tower farm
+    if(insideOf(4208,417,4223,479,x,z)){
+        return "McCocoaTower.js"
+    }
+    
+    //Coords for Netherwart Tower farm
+    if(insideOf(2270,8095,2290,8145,x,z)){
+        return "McNetherwart.js"
+    }
+    //Coords for Twisting Vines Tower farm
+    if(insideOf(2290,8095,2340,8145,x,z)){
+        return "McTwistingVines.js"
+    }
+    //Coords for Nether Stem Tower farm
+    if(insideOf(2200,8095,2267,8145,x,z)){
+        return "McNetherStemTower.js"
+    }
+    
+    
+    //couldn't find a farm
+    return "null"
+}
+
 function restartFarmScripts(){
+    //Do location based farm restarts
+    farmName = locateFarm(Player.getPlayer().getX(),
+                          Player.getPlayer().getZ())
+    if(farmName == "null"){
+        return
+    }
+    else{
+        JsMacros.runScript(farmName)
+    }
+    
+    /*  
     if(GlobalVars.getBoolean("delayZealOak")){
         JsMacros.runScript("McOakHiTechChop.js")
     }
     if(GlobalVars.getBoolean("delayNetherStem")){
         JsMacros.runScript("McNetherStemTower.js")
     }
+    */
 }
+
 /*-------------------
    4 Program Start
 -------------------*/
@@ -66,19 +152,25 @@ while(!hasReconnected){
     
     //If a disconnect has occured due to a KillSnitchEngage snitch msg
     if(GlobalVars.getBoolean("killsnitch")){
+    
         //reset killswitch so reconnection will occur
         //next time player manually joins
         GlobalVars.putBoolean("killsnitch",false)
         Client.waitTick(20)
+        
+        //Don't reconnect further down
         break
     }
     
     //check to see if we are loaded in already
     //meant to prevent having multiple scripts spam connecting
     if(!World.isWorldLoaded()){
+        //Client.connect("mini.civmc.net")
         Client.connect("play.civmc.net")
     }
     else{
+    
+        //We have connected to a world
         hasReconnected = true
         restartFarmScripts()
     }
