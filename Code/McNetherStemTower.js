b9583864e360feadb401337c96edec40bf16f85c
Greltam <101361088+Greltam@users.noreply.github.com>
2025-09-14 16:54:26 -0400

    Update McNetherStemTower.js

--- Code/McNetherStemTower.js
+++ Code/McNetherStemTower.js
@@ -1,48 +1,52 @@
-// Nether Stem Tower Script
+/*------------------------
+   0 Title Start
+------------------------*/
 /*
-#    !!! Script starts at 2264, 8097, 34 !!!
-    Find 1.2 Player Configurables to adjust for script restarts
-
-    Nether Stem Chopper on CivMC @ 2265, 8143, 7-40
-    Written by Greltam 1/12/2025
+    Name: GNC Nether Stem Tower Script
+    Location: CivMC @ 2264, 8146, 1
+    Author: Greltam
+    Date: 9/14/2025
     
-  Nether Stems: Crimson and Warped are very valuable. Their
-  cultivation is difficult, needing to control wart block growth
-  to expedite stem harvesting. A Tower with full saturation of 
-  growth spaces for wart blocks has been built. 
-  
-  Players will chop 1 layer at a time, back and forth from starting
-  north and south positions. Each layer requires 16 rows be chopped
-  with 20 stems per row. Each tree will be: moved through plate-door
-  to tree, chop angled down between next(50.5*) plate/netherrack. Then
-  aim to the top(-77), switch to hoe to clear any wart blocks. Replant
-  as going, switch to bonemeal, bonemeal netherrack, place fungus.
-  Repeat until next row, turn around an clear next row. Toss stems to
-  Collector, then start next row.
+    Description: A tower full of nether stems!
+    
+    Directions:
+        Enter building, go north to lodestone inside 
+        Use lodestone @ 2265, 8097, 2 up to y34
+        Enter first tree cell
+        Activate farm script.
+        
+    Collector: Lower level of Nether Stem building
 */
+/*------------------------
+   0 Title End
+------------------------*/
+    
 /*------------------------
    0.1 Player Requirements to Start
 ------------------------*/
-//!!!IMPORTANT!!!
-//Clear any stray wart blocks present in front of all doors
-//on each row of each layer. (East side only)
-//!!!IMPORTANT!!!
-
-//Player must place axe in the rightmost hotbar(9)
-//Player must place hoe in the next rightmost hotbar(8)
-//Hoe should be high durability
-//Player must place fungus in next rightmost hotbar(7)
-//Player must place bonemeal in next rightmost hotbar(6)
-//Player must place sticks in the next rightmost hotbar(5)
-
-//Inventory will be filled with:
-//10 stacks of warped fungus
-//10 stacks of crimson fungus
-//2 stacks of bone blocks
-//2 stacks of bonemeal
-//2 e(4/5)u3 diamond axes
-//1 e5u3 diamond hoe
-
+/*
+    !!!IMPORTANT!!!
+    Clear any stray wart blocks present in front of all doors
+    on each row of each layer. (East side only)
+    !!!IMPORTANT!!!
+    
+    Inventory will be filled with:
+    20 stacks of warped fungus
+    2 stacks of bone blocks
+    3 stacks of bonemeal
+    2 e(4/5)u3 diamond axes
+    1 e5u3 diamond hoe
+    1 stack of porkchop
+    1 stack of sticks
+    
+    Player must place axe in the rightmost hotbar(9)
+    Player must place hoe in the next rightmost hotbar(8)
+    Hoe should be high durability
+    Player must place fungus in next rightmost hotbar(7)
+    Player must place bonemeal in next rightmost hotbar(6)
+    Player must place sticks in the next rightmost hotbar(5)
+    
+*/
 /*-----------------------
    0.1 Player Requirements to Start End
 -----------------------*/
@@ -53,6 +57,12 @@
 ------------------------*/
 const util = require("./McUtilityFile.js")
 
+const config = require("./McUConfigFile.js")
+config.initialize()
+
+const visual = require("./McUVisualizer.js")
+visual.clear()
+
 /*-----------------------
    1.1 Import Files End
 -----------------------*/
@@ -60,35 +70,92 @@ const util = require("./McUtilityFile.js")
 /*------------------------
    1.2 Player Configurables Start
 ------------------------*/
+//player control initialization
+quitKey = "key.keyboard.j" // default: "key.keyboard.j"
+leftKey = "key.keyboard.a" // default: "key.keyboard.a"
+rightKey = "key.keyboard.d" // default: "key.keyboard.d"
+forwardKey = "key.keyboard.w" // default: "key.keyboard.w"
+backwardKey = "key.keyboard.s" // default: "key.keyboard.s"
+useKey = "key.mouse.right" // default: "key.mouse.right"
+attackKey = "key.mouse.left" // default: "key.mouse.left"
+lodestoneUpKey = "key.keyboard.space" // default: "key.keyboard.space"
+lodestoneDownKey = "key.keyboard.left.shift" 
+    // default: "key.keyboard.left.shift"
+logDiscord = true // default: "true"
+verboseLog = false // default: "false"
+logoutOnCompletion = false // default: "false"
+
+quitKey = config.getString("quitKey", quitKey)
+leftKey = config.getString("leftKey", leftKey)
+rightKey = config.getString("rightKey", rightKey)
+forwardKey = config.getString("forwardKey", forwardKey)
+backwardKey = config.getString("backwardKey", backwardKey)
+useKey = config.getString("useKey", useKey)
+attackKey = config.getString("attackKey", attackKey)
+lodestoneUpKey = config.getString("lodestoneUpKey", lodestoneUpKey)
+lodestoneDownKey = config.getString("lodestoneDownKey", lodestoneDownKey)
+logDiscord = config.getBool("logDiscord", logDiscord)
+verboseLog = config.getBool("verboseLog", verboseLog)
+logoutOnCompletion = config.getBool("logoutOnCompletion", logoutOnCompletion)
+
+
+//alter the default quitkey from j to whatever you want.
+util.setQuitKey(quitKey) //default: util.setQuitKey("key.keyboard.j") 
+
+/*-----------------------
+   1.2 Player Configurables End
+-----------------------*/
+
+
+/*-----------------------
+   1.2 Player Configurables End
+-----------------------*/
+
+/*------------------------
+   2 Global Variables Start
+------------------------*/
+farmName = "GSEZNether Stem Tower"
+regrowthTime = 24 * 3600 //hours multiplied by seconds per hour
+harvestDuration = 350 //minutes to complete full harvest
+
+//Player starts script at this location
+//Top of the Tower, North side
+xStartPosition = 2265 
+zStartPosition = 8097
+yStartPosition = 34
+
+//west side of the stem farm
+xChopStartPosition = 2262
+
+xRowEndPosition = 2202
+zNorthStartPosition = 8098
+zSouthStartPosition = 8144.5 //the .5 is to account for player centering
+
+//Time for stem chopping
+floorCutTicks = 30
+upperCutTicks = 50
+
+nextCellMoveTicks = 30
+
 //set either to true to plant entire farm as one stem type
 plantAllCrimson = false //default: plantAllCrimson = false
 plantAllWarped = true //default: plantAllWarped = false
 
-
 //set item list and look vector for tossing items into collector
 util.setTossItemList(["minecraft:crimson_stem",
+    "minecraft:stripped_crimson_stem",
     "minecraft:nether_wart_block",
     "minecraft:warped_stem",
+    "minecraft:stripped_warped_stem",
     "minecraft:warped_wart_block",
     "minecraft:shroomlight"])
 util.setTossLookVector([-140,0])
-
-//alter the default quitkey from j to whatever you want.
-util.setQuitKey("key.keyboard.j") //default: util.setQuitKey("key.keyboard.j") 
-
-//If Player falls off bridges, stop script.
-//turn on when chopping trees, off when moving between tree layers
-util.setQuitFromFalling(false)
 
 //If Player wants to scuff harvest using just an axe, let them set to false
 usingHoe = true //default: usingHoe = true
 
 //In case of needing to restart mid tree chop
-//change starting layer/row/tree to your current spot
-//set restarting to true
-//Stand directly in front of tree trunk when restarting.
 restarting = false //default: restarting = false
-
 startingLayer = 1 //default: startingLayer = 1
 startingRow = 1 //default: startingRow = 1
 startingTree = 1 //default: startingTree = 1
@@ -101,33 +168,6 @@ layerHeight = 9 //default: layerHeight = 9
 
 rowWidth = 3 //default: rowWidth = 3
 treeCellLength = 3 //default: treeBridgeLength = 3
-
-/*-----------------------
-   1.2 Player Configurables End
------------------------*/
-
-/*------------------------
-   2 Global Variables Start
-------------------------*/
-farmName = "GSEZNether Stem Tower"
-regrowthTime = 24 * 3600 //hours multiplied by seconds per hour
-
-//Player starts script at this location
-//Top of the Tower, North side
-xStartPosition = 2265 
-zStartPosition = 8097
-yStartPosition = 34
-
-//west side of the stem farm
-xRowEndPosition = 2202
-zNorthStartPosition = 8098
-zSouthStartPosition = 8144.5 //the .5 is to account for player centering
-
-//Time for stem chopping
-floorCutTicks = 30
-upperCutTicks = 50
-
-nextCellMoveTicks = 30
 /*-----------------------
    2 Global Variables End
 -----------------------*/
@@ -256,13 +296,20 @@ function bonemealNetherrack(){
         
         //use bonemeal on block pitch = 70
         //keep trying if not used
+        attempts = 0
         while(itemNumber === heldItem.getCount()){
             if(util.checkQuit()){break}
             util.simpleMove(
-                "key.mouse.right", util.player.getYaw(), 70, 2
+                useKey, util.player.getYaw(), 70, 2
             )
             util.spinTicks(10)
             heldItem = util.getItemInSelectedHotbar()
+            
+            //give up trying to bonemeal if we just aren't getting it
+            attempts = attempts + 1
+            if(attempts >= 10){
+                break
+            }
             ////Chat.log("items: " + itemNumber + "heldItemCount: " + heldItem.getCount())
         }
     }
@@ -312,7 +359,7 @@ function plantFungus(layerNum, rowNum){
         
         //plant fungus on block pitch = 70
         util.simpleMove(
-            "key.mouse.right", util.player.getYaw(), 70, 5
+            useKey, util.player.getYaw(), 70, 5
         )
         
         //check if we actually planted a fungus
@@ -327,7 +374,7 @@ function plantFungus(layerNum, rowNum){
             util.selectHotbar(4)
             //right click fungus with stick
             util.simpleMove(
-                "key.mouse.right", util.player.getYaw(), 70, 5
+                useKey, util.player.getYaw(), 70, 5
             )
         }
     }
@@ -335,20 +382,55 @@ function plantFungus(layerNum, rowNum){
 }
 
 function chopTree(layer, row, tree){
- 
-    //!!! Need to add error correction for movement to next cell here
 
+    //figure out next tree destination location
+    xDestination = xStartPosition
+    yDestination = yStartPosition - ((layer-1) * layerHeight)
+    zDestination = zNorthStartPosition
     
-    //Move from start or tree to next cell
-    //East side row start, move west
-    if(row % 2 == 1){
-        util.simpleMove("key.mouse.right",90,0,5)
-        util.simpleMove("key.keyboard.w",90,0,nextCellMoveTicks)
+    //to account for sliding against the wall
+    zOffset = 0.2
+    if(layer % 2 == 0){
+        zOffset = -0.2
     }
-    //Weast side row start, move east
+    
+    //separate for even/odd rows
+    if(row % 2 == 1){//odd row, starting right going left
+        xDestination = 
+            xStartPosition - (tree * treeCellLength) + 0.63
+    }else{//even row, starting left going right
+        xDestination = xStartPosition - 
+            ((treesPerRow - tree + 1 ) * treeCellLength) - 0.63
+    }
+    
+    if(layer%2 == 1){
+        zDestination = 
+            zDestination + ((row-1) * rowWidth) + zOffset
+    }else{
+        zDestination = 
+            zDestination + ((rowsPerLayer - row) * rowWidth) + zOffset
+    }
+    
+    //open doors to move into next cell
+    if(row % 2 == 1){
+        util.simpleMove(useKey,90,0,5)
+    }
     else{
-        util.simpleMove("key.mouse.right",-90,0,5)
-        util.simpleMove("key.keyboard.w",-90,0,nextCellMoveTicks)
+        util.simpleMove(useKey,-90,0,5)
+    }
+    /*
+    Chat.log("Aiming for:" + xDestination + "x, " +
+        yDestination + "y, " + zDestination + "z")
+    Chat.log("currently at for:" + Player.getPlayer().getX() + "x, " +
+        Player.getPlayer().getY()  + "y, " +
+        Player.getPlayer().getZ()  + "z")
+    */
+    //try to move to the next tree, but if not, return false
+    //to tell tree chop loop to redo next tree
+    //Tried a while loop with call to chop last tree but got double recursion
+    if(!util.complexMoveToLocation([useKey],
+        xDestination, zDestination, yDestination, 0.3)){
+        return false
     }
         
     //chop tree
@@ -359,23 +441,23 @@ function chopTree(layer, row, tree){
     
     //remove wart if on bottom two blocks
     util.selectHotbar(7) //select hoe
-    util.simpleMove("key.mouse.left",pYaw, 50.5, 8)
-    util.simpleMove("key.mouse.left",pYaw, 75, 8)
+    util.simpleMove(attackKey,pYaw, 50.5, 8)
+    util.simpleMove(attackKey,pYaw, 75, 8)
     //eye and foot level logs
     util.selectHotbar(8) //select Axe
-    util.simpleMove("key.mouse.left",pYaw, 50.5, floorCutTicks)
+    util.simpleMove(attackKey,pYaw, 50.5, floorCutTicks)
     
     //upper level logs
-    util.simpleMove("key.mouse.left",pYaw, -77, upperCutTicks /2)
+    util.simpleMove(attackKey,pYaw, -77, upperCutTicks /2)
     //clear wart blocks
     util.selectHotbar(7) //select Hoe
-    util.simpleMove("key.mouse.left",pYaw, -77, floorCutTicks /2)
+    util.simpleMove(attackKey,pYaw, -77, floorCutTicks /2)
     //upper level logs
     util.selectHotbar(8) //select Axe
-    util.simpleMove("key.mouse.left",pYaw, -77, upperCutTicks /2)
+    util.simpleMove(attackKey,pYaw, -77, upperCutTicks /2)
     //clear wart blocks
     util.selectHotbar(7) //select Hoe
-    util.simpleMove("key.mouse.left",pYaw, -77, floorCutTicks /2)
+    util.simpleMove(attackKey,pYaw, -77, floorCutTicks /2)
 
     
     //add bonemealing and fungus planting.
@@ -389,12 +471,38 @@ function chopTree(layer, row, tree){
 -------------------*/
 
 /*-------------------
-   4 Program Start
+   3.9 Pre-Program Start
 -------------------*/
+//GUI overlay
+visual.fullText("farmName", farmName, 0xdddddd,0,0)
+visual.fullText("toQuit", "Quit key: " + quitKey,0xffaaaa,0,8)
+visual.fullText("stems","Stems: " 
+    + util.getTossedItemAmount("minecraft:warped_stem"), 0x44ffdd,0,16)
+visual.fullText("timeLeft",
+            "Remaining time: " + harvestDuration, 0x999999,0,24)
+
+//restart farm on reconnect
+GlobalVars.putBoolean("farmRunning",true)
+
+
 Chat.log(greetingsText)
 Chat.log(quitText)
-util.logScriptStart(farmName)
+    
+//output to Discord
+if(logDiscord){
+    util.logScriptStart(farmName)
+}
 
+//protect from tabbed out dysfunction
+Client.grabMouse()
+
+/*-------------------
+   3.9 Pre-Program End
+-------------------*/
+
+/*-------------------
+   4 Program Start
+-------------------*/
 setStartingPosition()
 
 //chop all the layers
@@ -411,44 +519,50 @@ for(let i = startingLayer; i <= totalLayers; i++){
         for(let l = startingTree; l <= treesPerRow; l++){
             if(util.checkQuit()){break}
             while(!chopTree(i,j,l)){
-            //Needs error correction for mischopped cells
-            /*    util.simpleMove("key.keyboard.s",
+                //failure to chop tree, try resetting to previous
+                util.simpleMove(backwardKey,
                     Player.getPlayer().getYaw(),
                     Player.getPlayer().getPitch(),
                     40)
                 chopTree(i,j,l-1)
             }
-            */
-            }
+            visual.setText("timeLeft", "Remaining time: " 
+                + util.remainingMinutes(
+                i,j*l,totalLayers,
+                rowsPerLayer*treesPerRow,harvestDuration))
         }
         
         //done chopping east-west row, move to next row
         if(j % 2 == 1){
             //open door and move to end of row
-            util.simpleMove("key.mouse.right",90,0,5)
-            util.simpleMove("key.keyboard.w", 90,0,50)
+            util.simpleMove(useKey,90,0,5)
+            util.simpleMove(forwardKey, 90,0,50)
             
             //On a North-South layer
             if(i % 2 == 1){
                 //move to start of next of row
-                util.simpleMove("key.keyboard.w", 0,0,50)
+                util.simpleMove(forwardKey, 0,0,50)
             }
             //On a South-North layer
             else{
                 //move to start of next of row
-                util.simpleMove("key.keyboard.w", 180,0,50)
+                util.simpleMove(forwardKey, 180,0,50)
             }
         }
         //done chopping west-east row
         else{ 
             //open door and move to end of row
-            util.simpleMove("key.mouse.right",-90,0,5)
-            util.simpleMove("key.keyboard.w", -90,0,50)
+            util.simpleMove(useKey,-90,0,5)
+            //hoe any leftover leaves
+            util.selectHotbar(7) //select hoe
+            util.complexMove([forwardKey,attackKey],
+                -90, 0, 50)
             
             //On a North-South layer
             if(i % 2 == 1){
                 //move to start of next of row
-                util.simpleMove("key.keyboard.w", 0,0,50)
+                util.complexMove([forwardKey,attackKey],
+                    0, 0, 50)
                 //toss logs into collector
                 //but not on the last row(no chute)
                 if(j != rowsPerLayer){util.tossItems()}
@@ -458,8 +572,11 @@ for(let i = startingLayer; i <= totalLayers; i++){
                 //toss logs into collector
                 util.tossItems()  
                 //move to start of next of row
-                util.simpleMove("key.keyboard.w", 180,0,50)
+                util.complexMove([forwardKey,attackKey],
+                    180, 0, 50)
             }
+            visual.setText("stems", "Stems: " 
+                + util.getTossedItemAmount("minecraft:warped_stem"))
             //Last row needs to get to lodestone                     
             if(j == rowsPerLayer){
                 break
@@ -479,21 +596,21 @@ for(let i = startingLayer; i <= totalLayers; i++){
     //On a North-South layer
     if(i % 2 == 1){
         //move into chute
-        util.simpleMove("key.keyboard.w", 0,0,50)
+        util.simpleMove(forwardKey, 0,0,50)
         //wait for player to fall past ladder
         util.simpleMove("", 0,0,50)
         //align to start of row
-        util.simpleMove("key.keyboard.w", 180,0,50)
+        util.simpleMove(forwardKey, 180,0,50)
     }
     //On a South-North layer
     else{        
         //move into chute
-        util.simpleMove("key.keyboard.w", 180,0,50)
-        util.simpleMove("key.keyboard.w", 90,0,50)
+        util.simpleMove(forwardKey, 180,0,50)
+        util.simpleMove(forwardKey, 90,0,50)
         //wait for player to fall past ladder
         util.simpleMove("", 0,0,50)
         //align to start of row
-        util.simpleMove("key.keyboard.w", 0,0,50)
+        util.simpleMove(forwardKey, 0,0,50)
     }
     
     //if restarting, set restart to false and change starting back to defaults
@@ -503,26 +620,37 @@ for(let i = startingLayer; i <= totalLayers; i++){
         startingTree = 1
     }
 }
+/*-------------------
+   4 Program End
+-------------------*/
+
+/*-------------------
+   4.1 Shutdown Start
+-------------------*/
+
+//prevent reconnect from restarting farm
+GlobalVars.putBoolean("farmRunning", false)
 
 //Reset keybinds to prevent phantom key holds.
 util.resetKeys()
 
+//log script completion
 Chat.log(finishedText)
-util.logScriptEnd(farmName, regrowthTime)
 
-//if we restarted script over night logout because we are
-//probably still asleep.
-if(GlobalVars.getBoolean("delayFarm")){
-
-    GlobalVars.putBoolean("delayFarm",false)
-    GlobalVars.putBoolean("delayNetherStem",false)
-    
-    GlobalVars.putBoolean("killsnitch", true)
-    Chat.say("/logout")
-    Client.waitTick(400)
-    Client.disconnect() 
+//output to Discord
+if(logDiscord){
+    util.logScriptEnd(farmName, regrowthTime, verboseLog)
 }
 
+//clear all GUI overlays
+visual.clear()
+
+//Exit server if on a delay start or desired
+if(logoutOnCompletion || GlobalVars.getBoolean("delayFarm")){
+    GlobalVars.putBoolean("delayFarm", false)
+    GlobalVars.putBoolean("killsnitch", true)
+    Chat.say("/logout")
+}
 /*-------------------
-   4 Program End
+   4.1 Shutdown End
 -------------------*/
