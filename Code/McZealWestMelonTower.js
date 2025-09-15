d072c8801bc24750d57046ff13d2e3420ceb721c
Greltam <101361088+Greltam@users.noreply.github.com>
2025-09-15 12:02:47 -0400

    reset killsnitch on startup

--- Code/McZealWestMelonTower.js
+++ Code/McZealWestMelonTower.js
@@ -1,4 +1,3 @@
-
 /*------------------------
    0 Title Start
 ------------------------*/
@@ -273,6 +272,7 @@ visual.fullText("timeLeft",
 
 //restart farm on reconnect
 GlobalVars.putBoolean("farmRunning",true)
+GlobalVars.putBoolean("killsnitch", false)
 
 
 Chat.log(greetingsText)
