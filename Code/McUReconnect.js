4c2bce4086701a3b986e5c975639859cd3108ee9
Greltam <101361088+Greltam@users.noreply.github.com>
2025-12-10 21:35:49 -0500

    add Beet Tower to restart on reconnect

--- Code/McUReconnect.js
+++ Code/McUReconnect.js
@@ -119,45 +119,61 @@ function locateFarm(x,z){
         GlobalVars.putBoolean("daisySata",false)
         return "vDaisyMelonPotato.js"
     }
+    
+    //**********//
+    //GSEZ Farms//
+    //**********//
+    
     //Coords for Oak farm
     if(insideOf(2863,5114,2959,5203,x,z)){
         return "McOakHiTechChop.js"
     }
-    
     //Coords for Carrot farm
     if(insideOf(2974,5105,3007,5138,x,z)){
         return "McCarrotTower.js"
     }
-    
     //Coords for Wheat farm
     if(insideOf(3021,5105,3055,5139,x,z)){
         return "McWheatTower.js"
     }
     
+    //**********//
+    //SaTa Farms//
+    //**********//
+    
     //Coords for McBeachaEastMelonTower farm
     if(insideOf(3994,7337,4021,7380,x,z)){
         return "McBeachaEastMelonTower.js"
     }
-    
     //Coords for Potato Tower farm
     if(insideOf(3994,7386,4024,7415,x,z)){
         return "McPotatoTower.js"
     }
-    
     //Coords for Sweet berry farm
     if(insideOf(3949,7426,3984,7457,x,z)){
         return "McSweetBerry.js"
     }
-    
+    //Coords for Beetroot farm
+    if(insideOf(3981,7462,4017,7495,x,z)){
+        return "McBeetTower.js"
+    }
     //Coords for Zeal Melon farm
     if(insideOf(3952,7372,3988,7390,x,z)){
         return "McZealWestMelonTower.js"
     }
     
+    //**********//
+    //GSEX Farms//
+    //**********//
+    
     //Coords for Cocoa Tower farm
     if(insideOf(4208,417,4223,479,x,z)){
         return "McCocoaTower.js"
     }
+    
+    //**********//
+    //GNC Farms//
+    //**********//
     
     //Coords for Netherwart Tower farm
     if(insideOf(2270,8095,2290,8145,x,z)){
