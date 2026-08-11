999ae530aed5d524a5423ade5e1a134efbfeaec2
Greltam <101361088+Greltam@users.noreply.github.com>
2026-08-11 00:49:33 -0400

    update layers for new expansion

--- Code/McJungleHiTechChop.js
+++ Code/McJungleHiTechChop.js
@@ -112,12 +112,12 @@ util.setQuitKey(quitKey) //default: util.setQuitKey("key.keyboard.j")
 
 farmName = "GSEx Jungle Tower"
 regrowthTime = 19 * 3600 //hours multiplied by seconds per hour
-harvestDuration = 52 //minutes to run a full harvest
+harvestDuration = 156 //minutes to run a full harvest
 
 //Player starts script at this location
 xStartPosition = 4202 
 zStartPosition = 417
-yStartPosition = 72
+yStartPosition = 0
 
 //west side of the tree farm, beginning of chopping trees
 xChopStartPosition = 4158
@@ -147,7 +147,7 @@ startingRow = 1 //default: startingRow = 1
 startingTree = 1 //default: startingTree = 1
 
 //total layers in the tree farm
-totalLayers = 4 //default: totalLayers = 8
+totalLayers = 12 //default: totalLayers = 8
 treesPerRow = 8 //default: treesPerRow = 16
 rowsPerLayer = 8 //default: rowsPerLayer = 16
 layerHeight = 12 //default: layerHeight = 12
